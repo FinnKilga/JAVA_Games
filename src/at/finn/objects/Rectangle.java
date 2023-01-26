f3203e70a260a0e014349a0904cf5fe0ed6f1e5e
FinnKilga <finn.kilga@student.htldornbirn.at>
2023-01-26 09:28:32 +0100

    Analyse fertig

---
+++ src/at/finn/objects/Rectangle.java
@@ -0,0 +1,28 @@
+package at.finn.objects;
+import org.newdawn.slick.*;
+public class Rectangle
+{
+    private float x;
+    private float y;
+    private float speed;
+
+    public Rectangle(float x, float y, float speed)
+    {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    public void render(Graphics graphics)
+    {
+        graphics.drawRect(this.x,this.y,20,20);
+    }
+    public void update(int delta)
+    {
+        this.x += (float)delta/this.speed;
+        if (this.x > 780)
+        {
+            this.x = 0;
+        }
+    }
+}
